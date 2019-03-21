a198b28d4ebc1049d0db1cad23a33bc862d81082
Alena Dzeikun <Alena.Dzeikun@itechart-group.com>
2019-03-21 14:27:30 +0300

    Added igdb module for igdb access

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='igdb_api',
+    version='0.0.1',
+    license='MIT',
+    author='Dejkun Elena',
+    author_email='dejkun.elena.9@gmail.com',
+    install_requires=[
+        'requests'
+    ]
+)
