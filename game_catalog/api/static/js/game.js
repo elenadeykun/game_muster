eab8d322f65591afa1747d1935b050ab113a7cb7
Alena Dzeikun <Alena.Dzeikun@itechart-group.com>
2019-03-22 17:55:56 +0300

    Updated templates with filter search logic

--- game_catalog/api/static/js/game.js
+++ game_catalog/api/static/js/game.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-    console.log($('.screen-img'));
     $('.screen-img').each(function(i,elem){
         elem.src = $(this).data("url");
     });
--- game_catalog/api/static/js/script.js
+++ game_catalog/api/static/js/script.js
@@ -1,8 +1,93 @@
 $(document).ready(function(){
     $('.game__img').each(function(i,elem){
-
         if($(this).data("url")){
             elem.src = $(this).data("url");
         }
     });
+
+    $('form[name=login-form]').submit(function(event){
+        event.preventDefault();
+
+        $.ajax({
+            url: '/login',
+            method: 'POST',
+            data: $('form[name=login-form]').serialize(),
+            dataType: 'json',
+            success: function (data) {
+               $('a[name=close]')[0].click();
+            }
+         });
+    });
+
+    $('a[name=logout').click( function(){
+
+        $.ajax({
+          url: '/logout',
+          dataType: 'json',
+          success: function (data) {
+              alert(data.stat);
+            }
+         });
+    });
+
+    $('form[name=filter-form]').submit(function(event){
+        event.preventDefault();
+
+        $.ajax({
+            url: '/ajax/filter',
+            method: "GET",
+            data: $('form[name=filter-form]').serialize(),
+            dataType: 'json',
+            success: function (data) {
+               games = data.games;
+               games_container = document.getElementById("games");
+               // ???
+               games_container.innerHTML= "";
+               fragment = document.createDocumentFragment();
+               games.forEach(function(elem, i){
+                    game = createGameElement(elem);
+                    fragment.appendChild(game);
+               })
+               games_container.appendChild(fragment);
+            }
+         });
+    });
 });
+
+function createGameElement(gameData){
+    game = document.createElement('div');
+    game.classList.add('game__container');
+    description = document.createElement('span');
+    description.classList.add('game__body');
+
+    title = document.createElement('h4');
+    title.classList.add('game__title')
+    title.textContent = gameData.name;
+
+    linkContainer = document.createElement('p');
+    link = document.createElement('a');
+    link.classList.add('game__link');
+    link.href = "game/" + gameData.id;
+    link.textContent = 'Open';
+    linkContainer.appendChild(link);
+    mustLink = link.cloneNode();
+    mustLink.href = "#" ;
+    mustLink.textContent = 'Must';
+    linkContainer.appendChild(mustLink);
+
+    description.appendChild(title);
+    description.appendChild(linkContainer);
+    game.appendChild(description);
+    img = document.createElement("img");
+    img.classList.add("game__img");
+
+    if(!!gameData.screenshots){
+       img.src = gameData.screenshots[0].url;
+    } else {
+       img.src = "/static/media/pacman.jpg";
+    }
+
+    console.log(gameData.id);
+    game.appendChild(img);
+    return game;
+}
