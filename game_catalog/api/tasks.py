11a742431ca65ac6771f83551806445eef03741b
Elena <dejkun.elena.9@gmail.com>
2019-04-09 10:01:05 +0300

    Update celery task

--- game_catalog/api/tasks.py
+++ game_catalog/api/tasks.py
@@ -17,46 +17,53 @@ def save_games():
     offset = Game.objects.all().count() // LIMIT
 
     games = igdb.get_games(offset=offset, limit=LIMIT)
+    task = Downloader()
     if games:
         for game in games:
-            save_game(game)
+            task.save(game)
 
 
-def save_parameter(game, model, parameter, value):
-    elements = []
+class Downloader:
+    def __save_instance(self, game, model, parameter, value):
+        elements = []
 
-    if game.get(parameter):
-        for elem in game.get(parameter):
-            if elem.get(value):
-                elem_db, created = model.objects.get_or_create(name=elem[value])
-                elements.append(elem_db)
+        if game.get(parameter):
+            for elem in game.get(parameter):
+                if elem.get(value):
+                    elem_db, created = model.objects.get_or_create(name=elem[value])
+                    elements.append(elem_db)
 
-    return elements
+        return elements
 
+    def __save_genres(self, game):
+        return self.__save_instance(game, Genre, 'genres', 'name')
 
-def save_game(game):
-    logging.debug(game.get('release_dates'))
-    release_date = (datetime.fromtimestamp(game['release_dates'][-1]['date'])
-                    if 'release_dates' in game and game['release_dates'][-1].get('date') else None)
+    def __save_platforms(self, game):
+        return self.__save_instance(game, Platform, 'platforms', 'abbreviation')
 
-    game_db, created = Game.objects.get_or_create(name=game['name'], description=game.get('summary'),
-                                                  release_date=release_date,
-                                                  users_rating=float(game.get('rating', 0)),
-                                                  critics_rating=float(game.get('aggregated_rating', 0)),
-                                                  users_views=int(game.get('rating_count', 0)),
-                                                  critics_views=int(game.get('aggregated_rating_count', 0)))
+    def save(self, game):
+        logging.debug(game.get('release_dates'))
+        release_date = (datetime.fromtimestamp(game['release_dates'][-1]['date'])
+                        if 'release_dates' in game and game['release_dates'][-1].get('date') else None)
 
-    if created:
-        genres = save_parameter(game, Genre, 'genres', 'name')
-        platforms = save_parameter(game, Platform, 'platforms', 'abbreviation')
-        game_db.genres.set(genres)
-        game_db.platforms.set(platforms)
-        game_db.save()
+        game_db, created = Game.objects.get_or_create(name=game['name'], description=game.get('summary'),
+                                                      release_date=release_date,
+                                                      users_rating=float(game.get('rating', 0)),
+                                                      critics_rating=float(game.get('aggregated_rating', 0)),
+                                                      users_views=int(game.get('rating_count', 0)),
+                                                      critics_views=int(game.get('aggregated_rating_count', 0)))
 
-        screenshots = []
+        if created:
+            genres = self.__save_genres(game)
+            platforms = self.__save_platforms(game)
+            game_db.genres.set(genres)
+            game_db.platforms.set(platforms)
+            game_db.save()
 
-        if game.get('screenshots'):
-            for screen in game.get('screenshots'):
-                screen_db, created = Image.objects.get_or_create(url=screen['url'], game=game_db)
-                screenshots.append(screen_db)
+            screenshots = []
+
+            if game.get('screenshots'):
+                for screen in game.get('screenshots'):
+                    screen_db, created = Image.objects.get_or_create(url=screen['url'], game=game_db)
+                    screenshots.append(screen_db)
 
