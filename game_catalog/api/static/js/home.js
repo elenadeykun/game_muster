8a5fc0255774826079e83bf2e4d71821e4f15891
Elena <dejkun.elena.9@gmail.com>
2019-04-11 12:17:19 +0300

    Add style files to api app

---
+++ game_catalog/api/static/js/home.js
@@ -0,0 +1,80 @@
+"use strict";
+
+window.onscroll = function() {
+    if(window.pageYOffset + window.innerHeight >= document.body.clientHeight){
+        var gamesContainer = document.getElementById("games");
+        
+        if (gamesContainer.dataset.filter === "true"){
+            sendFilterQuery();
+        } else {
+            var page = parseInt(gamesContainer.dataset.page);
+
+
+            createQuery("GET","/get-particle-games/" + page, function(response){
+                var games = response.games;
+                if(games){
+                    var fragment = document.createDocumentFragment();
+                    games.forEach(function(elem, i){
+                        var game = createGameElement(elem);
+                        fragment.appendChild(game);
+                    })
+                    gamesContainer.appendChild(fragment);
+                }
+            });
+        }
+
+        gamesContainer.dataset.page = page + 1;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+
+     document.getElementById("search-field").addEventListener("keydown", function(event){
+        var ENTER_KEY_CODE = 13;
+
+        if (event.keyCode === ENTER_KEY_CODE) {
+            getGamesBySearch();
+         }
+     });
+
+     var search = document.querySelector("button[name=search]");
+     search.addEventListener('click', function(event){
+        getGamesBySearch();
+    });
+
+    var elem = document.querySelector('input[type="range"]');
+
+    var rangeValue = function(){
+      var newValue = elem.value;
+      var target = document.querySelector('.value');
+      target.innerHTML = newValue;
+    }
+
+    elem.addEventListener("input", rangeValue);
+});
+
+function getGamesBySearch(){
+     if(!document.getElementById("search-field").value){
+            showMessage("Search string is empty.");
+     } else {
+        createQuery("GET", "/search/" + document.getElementById("search-field").value, function(response){
+            var games = response.games;
+            var gamesContainer = document.getElementById("games");
+            gamesContainer.innerHTML= "";
+
+            if (games.length > 0){
+                var fragment = document.createDocumentFragment();
+                for (var i = 0; i < games.length; i++){
+                    var game = createGameElement(games[i]);
+                    fragment.appendChild(game);
+                }
+                gamesContainer.appendChild(fragment);
+            } else {
+                var message = document.createElement("h1");
+                message.innerText = "Not found.";
+                message.classList.add("message__text");
+                gamesContainer.appendChild(message);
+            }
+        });
+     }
+}
---
+++ game_catalog/api/static/js/must.js
@@ -0,0 +1,32 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function(){
+
+    var removeLinks = document.querySelectorAll("button[name=create]");
+
+    for (var i = 0; i < removeLinks.length; i++) {
+        removeLinks[i].addEventListener("click", function(event){
+            event.preventDefault();
+
+            var action = this.dataset.action;
+
+            createQuery("GET", action, function(response){
+
+                var link = document.querySelector("button[data-action='" + action + "']");
+                if (action.match("remove-must")){
+                    action = action.replace("remove-must", "create-must");
+                    link.classList.add("remust-link");
+                    link.innerText = "ReMUST";
+                    link.dataset.action =  action;
+               } else {
+                    action = action.replace("create-must", "remove-must");
+                    link.classList.remove("remust-link");
+                    link.classList.add("must-link");
+                    link.innerText = "UnMUST";
+                    link.dataset.action = action;
+               }
+               showMessage(response.Status);
+            })
+        });
+    }
+});
